0ba8875b229abd00ea5da18e0cf7e41ce08bcf2d
Ennio Barbaro <enniobarbaro@gmail.com>
2015-07-07 00:16:05 +0200

    Process commands only if the phase is xplm_CommandBegin

--- x-plane-plugin/plugin.c
+++ x-plane-plugin/plugin.c
@@ -114,6 +114,8 @@ static int TrackToggleHandler( XPLMCommandRef inCommand,
                                XPLMCommandPhase inPhase,
                                void * inRefCon )
 {
+    if ( inPhase != xplm_CommandBegin )
+        return 0;
     if ( track_disabled )
     {
         //Enable
@@ -136,6 +138,9 @@ static int TranslationToggleHandler( XPLMCommandRef inCommand,
                                      XPLMCommandPhase inPhase,
                                      void * inRefCon )
 {
+    if ( inPhase != xplm_CommandBegin )
+        return 0;
+    
     translation_disabled = !translation_disabled;
     if (!translation_disabled)
     {
